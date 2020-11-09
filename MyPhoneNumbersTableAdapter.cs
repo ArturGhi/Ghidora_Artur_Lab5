286cca09956fd27ede222119b8d5582e7e7a7b21
ArturGhi <69517775+ArturGhi@users.noreply.github.com>
2020-11-09 12:15:16 +0200

    Add project files.

---
+++ MyPhoneNumbersTableAdapter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghidora_Artur_Lab5.PhoneNumbersDataSetTableAdapters
+{
+    public partial class PhoneNumbersTableAdapter
+    {
+        public PhoneNumbersTableAdapter(String connectionString)
+        {
+            this.ClearBeforeFill = true;
+            this.Connection = new System.Data.SqlClient.SqlConnection(connectionString);
+        }
+    }
+}
